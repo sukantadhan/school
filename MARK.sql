9d1037fccd2182a021dd1cb50d5be9c6cd67e604
sukantadhan <55803081+sukantadhan@users.noreply.github.com>
2019-10-20 12:03:47 +0530

    Added Oracle SQL script
    Added schema structure and data for Oracle DB

---
+++ MARK.sql
@@ -0,0 +1,47 @@
+--------------------------------------------------------
+--  File created - Sunday-October-20-2019   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table MARK
+--------------------------------------------------------
+
+  CREATE TABLE "SCOTT"."MARK" 
+   (	"ROLLNO" NUMBER, 
+	"SUBJECT" VARCHAR2(20 BYTE), 
+	"MARK" VARCHAR2(20 BYTE)
+   ) PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT)
+  TABLESPACE "USERS" ;
+REM INSERTING into SCOTT.MARK
+SET DEFINE OFF;
+Insert into SCOTT.MARK (ROLLNO,SUBJECT,MARK) values (3,'MIL','60');
+Insert into SCOTT.MARK (ROLLNO,SUBJECT,MARK) values (3,'Math','50');
+Insert into SCOTT.MARK (ROLLNO,SUBJECT,MARK) values (3,'English','90');
+Insert into SCOTT.MARK (ROLLNO,SUBJECT,MARK) values (3,'History','45');
+Insert into SCOTT.MARK (ROLLNO,SUBJECT,MARK) values (3,'Geography','61');
+Insert into SCOTT.MARK (ROLLNO,SUBJECT,MARK) values (3,'Sanskrit','68');
+Insert into SCOTT.MARK (ROLLNO,SUBJECT,MARK) values (3,'Science','91');
+Insert into SCOTT.MARK (ROLLNO,SUBJECT,MARK) values (2,'MIL','42');
+Insert into SCOTT.MARK (ROLLNO,SUBJECT,MARK) values (2,'Math','56');
+Insert into SCOTT.MARK (ROLLNO,SUBJECT,MARK) values (2,'English','70');
+Insert into SCOTT.MARK (ROLLNO,SUBJECT,MARK) values (2,'History','95');
+Insert into SCOTT.MARK (ROLLNO,SUBJECT,MARK) values (2,'Geography','51');
+Insert into SCOTT.MARK (ROLLNO,SUBJECT,MARK) values (2,'Sanskrit','58');
+Insert into SCOTT.MARK (ROLLNO,SUBJECT,MARK) values (2,'Science','81');
+Insert into SCOTT.MARK (ROLLNO,SUBJECT,MARK) values (1,'MIL','12');
+Insert into SCOTT.MARK (ROLLNO,SUBJECT,MARK) values (1,'Math','86');
+Insert into SCOTT.MARK (ROLLNO,SUBJECT,MARK) values (1,'English','50');
+Insert into SCOTT.MARK (ROLLNO,SUBJECT,MARK) values (1,'History','25');
+Insert into SCOTT.MARK (ROLLNO,SUBJECT,MARK) values (1,'Geography','61');
+Insert into SCOTT.MARK (ROLLNO,SUBJECT,MARK) values (1,'Sanskrit','38');
+Insert into SCOTT.MARK (ROLLNO,SUBJECT,MARK) values (1,'Science','41');
+--------------------------------------------------------
+--  Constraints for Table MARK
+--------------------------------------------------------
+
+  ALTER TABLE "SCOTT"."MARK" MODIFY ("ROLLNO" NOT NULL ENABLE);
+ 
+  ALTER TABLE "SCOTT"."MARK" MODIFY ("SUBJECT" NOT NULL ENABLE);
+ 
+  ALTER TABLE "SCOTT"."MARK" MODIFY ("MARK" NOT NULL ENABLE);
---
+++ STUDENTS.sql
@@ -0,0 +1,35 @@
+--------------------------------------------------------
+--  File created - Sunday-October-20-2019   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table STUDENTS
+--------------------------------------------------------
+
+  CREATE TABLE "SCOTT"."STUDENTS" 
+   (	"ROLLNO" NUMBER, 
+	"NAME" VARCHAR2(20 BYTE), 
+	"EMAIL" VARCHAR2(20 BYTE), 
+	"USERNAME" VARCHAR2(20 BYTE), 
+	"PASSWORD" VARCHAR2(20 BYTE)
+   ) PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT)
+  TABLESPACE "USERS" ;
+REM INSERTING into SCOTT.STUDENTS
+SET DEFINE OFF;
+Insert into SCOTT.STUDENTS (ROLLNO,NAME,EMAIL,USERNAME,PASSWORD) values (1,'Pratap Kumar Yadav','pratap@yahoo.com','pcdhan','password');
+Insert into SCOTT.STUDENTS (ROLLNO,NAME,EMAIL,USERNAME,PASSWORD) values (2,'Anil Kumar Sharma','anil@hotmail.com','anil','password');
+Insert into SCOTT.STUDENTS (ROLLNO,NAME,EMAIL,USERNAME,PASSWORD) values (3,'Pascal Smith','pascal@gmail.com','pascal','password@123');
+--------------------------------------------------------
+--  Constraints for Table STUDENTS
+--------------------------------------------------------
+
+  ALTER TABLE "SCOTT"."STUDENTS" MODIFY ("ROLLNO" NOT NULL ENABLE);
+ 
+  ALTER TABLE "SCOTT"."STUDENTS" MODIFY ("NAME" NOT NULL ENABLE);
+ 
+  ALTER TABLE "SCOTT"."STUDENTS" MODIFY ("EMAIL" NOT NULL ENABLE);
+ 
+  ALTER TABLE "SCOTT"."STUDENTS" MODIFY ("USERNAME" NOT NULL ENABLE);
+ 
+  ALTER TABLE "SCOTT"."STUDENTS" MODIFY ("PASSWORD" NOT NULL ENABLE);
